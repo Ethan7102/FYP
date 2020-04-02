4192c07f6d9f6b04c8d5188becd130657a729ae8
Ethan7102 <51076587+Ethan7102@users.noreply.github.com>
2020-04-02 20:15:01 +0800

    gg

--- application/windowApp/main/Drone.py
+++ application/windowApp/main/Drone.py
@@ -5,8 +5,8 @@ class Drone:
     def __init__(self,connection_string):
         self.connection_string=connection_string
         #self.sitl = dronekit_sitl.start_default()
-        self.drone = connect(self.connection_string, wait_ready=True)
-        #self.drone = connect('tcp:127.0.0.1:5760', wait_ready=True)
+        #self.drone = connect(self.connection_string, wait_ready=True)
+        self.drone = connect('tcp:127.0.0.1:5760', wait_ready=True)
 
     def connectDrone(self):
         self.drone = connect(self.connection_string, wait_ready=True)
